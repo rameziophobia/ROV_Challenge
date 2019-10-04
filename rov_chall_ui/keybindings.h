d174fb2ce7867e3dff8fb1e6e74c482d78b46fe2
George Gabra Jr <george.ayad@icloud.com>
2019-10-05 01:41:51 +0200

    updated key bindings
    - Added JSON settings file
    - Support for saving and loading multiple settings files
    - Emits signal to update settings from file
    - Needs wait for sdl function and read from sdl function

--- rov_chall_ui/keybindings.h
+++ rov_chall_ui/keybindings.h
@@ -3,6 +3,12 @@
 
 #include <QDialog>
 #include <QDebug>
+#include <QFileDialog>
+#include <QFile>
+#include <QDir>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonDocument>
 
 namespace Ui {
 class keyBindings;
@@ -17,9 +23,30 @@ public:
     ~keyBindings();
 
 private slots:
+    void loadData();
+    void on_close_dialogButtonBox_accepted();
+
+    void on_pushButton1_clicked();
+    void on_pushButton2_clicked();
+    void on_pushButton3_clicked();
+    void on_pushButton4_clicked();
+    void on_pushButton5_clicked();
+    void on_pushButton6_clicked();
+    void on_pushButton7_clicked();
+    void on_pushButton8_clicked();
+    void on_pushButton9_clicked();
+    void on_pushButton10_clicked();
+    void on_pushButton11_clicked();
+    void on_pushButton12_clicked();
 
 private:
     Ui::keyBindings *ui;
+    QJsonDocument jsonDocument;
+    QJsonObject json;
+
+signals:
+    void updatedKeyBindings();
+
 };
 
 #endif // KEYBINDINGS_H
