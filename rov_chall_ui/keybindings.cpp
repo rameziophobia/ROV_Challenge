b3421f0bd4535fd4251e61731b9e675a7602e8ec
George Gabra Jr <george.ayad@icloud.com>
2019-10-05 01:41:51 +0200

    updated key bindings
    - Added JSON settings file
    - Support for saving and loading multiple settings files
    - Emits signal to update settings from file
    - Needs wait for sdl function and read from sdl function

--- rov_chall_ui/keybindings.cpp
+++ rov_chall_ui/keybindings.cpp
@@ -1,14 +1,142 @@
 #include "keybindings.h"
 #include "ui_keybindings.h"
 
-keyBindings::keyBindings(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::keyBindings)
-{
+keyBindings::keyBindings(QWidget *parent) : QDialog(parent), ui(new Ui::keyBindings){
     ui->setupUi(this);
+    loadData();
 }
 
 keyBindings::~keyBindings()
 {
     delete ui;
+}
+
+void keyBindings::loadData(){
+    QString openFileName = QFileDialog::getOpenFileName(this, tr("Open Settings File"), QString(QDir::currentPath()), tr("JSON (*.json)"));
+    QFileInfo fileInfo(openFileName);   // Using QFileInfo
+    QDir::setCurrent(fileInfo.path());  // set the current working directory where the file will be
+    // Create a file object and open it for reading.
+    QFile jsonFile(openFileName);
+    if (!jsonFile.open(QIODevice::ReadOnly))return; //should handle error here
+    // Read the entire file
+    QByteArray saveData = jsonFile.readAll();
+    // Create QJsonDocument
+    jsonDocument = QJsonDocument::fromJson(saveData);
+    // From which we select an object into the current working QJsonObject
+    json = jsonDocument.object();
+
+    //load data from json file
+    ui->pushButton1->setText(json["action1"].toString());
+    ui->pushButton2->setText(json["action2"].toString());
+    ui->pushButton3->setText(json["action3"].toString());
+    ui->pushButton4->setText(json["action4"].toString());
+    ui->pushButton5->setText(json["action5"].toString());
+    ui->pushButton6->setText(json["action6"].toString());
+    ui->pushButton7->setText(json["action7"].toString());
+    ui->pushButton8->setText(json["action8"].toString());
+    ui->pushButton9->setText(json["action9"].toString());
+    ui->pushButton10->setText(json["action10"].toString());
+    ui->pushButton11->setText(json["action11"].toString());
+    ui->pushButton12->setText(json["action12"].toString());
+}
+
+void keyBindings::on_close_dialogButtonBox_accepted()
+{
+    QString saveFileName = QFileDialog::getSaveFileName(this, tr("Save Settings File"), QString(QDir::currentPath()), tr("JSON (*.json)"));
+    QFileInfo fileInfo(saveFileName);   // Using QFileInfo
+    QDir::setCurrent(fileInfo.path());  // set the current working directory where the file will be, otherwise it may not work
+
+    // Create a file object and open it for writing.
+    QFile jsonFile(saveFileName);
+    if (!jsonFile.open(QIODevice::WriteOnly))return;
+
+    // Write the current Json object to a file.
+    jsonFile.write(QJsonDocument(json).toJson(QJsonDocument::Indented));
+    jsonFile.close();   // Close file
+
+    emit updatedKeyBindings();
+}
+
+void keyBindings::on_pushButton1_clicked()
+{
+    //wait for sdl event
+    QString sdl = "sdl input";
+    json["action1"] = sdl;
+}
+
+void keyBindings::on_pushButton2_clicked()
+{
+    //wait for sdl event
+    QString sdl = "sdl input";
+    json["action2"] = sdl;
+}
+
+void keyBindings::on_pushButton3_clicked()
+{
+    //wait for sdl event
+    QString sdl = "sdl input";
+    json["action3"] = sdl;
+}
+
+void keyBindings::on_pushButton4_clicked()
+{
+    //wait for sdl event
+    QString sdl = "sdl input";
+    json["action4"] = sdl;
+}
+
+void keyBindings::on_pushButton5_clicked()
+{
+    //wait for sdl event
+    QString sdl = "sdl input";
+    json["action5"] = sdl;
+}
+
+void keyBindings::on_pushButton6_clicked()
+{
+    //wait for sdl event
+    QString sdl = "sdl input";
+    json["action6"] = sdl;
+}
+
+void keyBindings::on_pushButton7_clicked()
+{
+    //wait for sdl event
+    QString sdl = "sdl input";
+    json["action7"] = sdl;
+}
+
+void keyBindings::on_pushButton8_clicked()
+{
+    //wait for sdl event
+    QString sdl = "sdl input";
+    json["action8"] = sdl;
+}
+
+void keyBindings::on_pushButton9_clicked()
+{
+    //wait for sdl event
+    QString sdl = "sdl input";
+    json["action9"] = sdl;
+}
+
+void keyBindings::on_pushButton10_clicked()
+{
+    //wait for sdl event
+    QString sdl = "sdl input";
+    json["action10"] = sdl;
+}
+
+void keyBindings::on_pushButton11_clicked()
+{
+    //wait for sdl event
+    QString sdl = "sdl input";
+    json["action11"] = sdl;
+}
+
+void keyBindings::on_pushButton12_clicked()
+{
+    //wait for sdl event
+    QString sdl = "sdl input";
+    json["action12"] = sdl;
 }
